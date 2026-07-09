da0cbaf43081dee2780c38dd1aa297251f44bebd
hwen <hongwei.wen@cern.ch>
2026-07-09 19:09:03 +0200

    a

---
+++ Jobs/scan_phipipi_20260707_063745/007_add_f2_1525_flatte/run_condor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euo pipefail
+
+cd "/afs/cern.ch/work/h/hwen/public/pwa/auto-init/Jobs/scan_phipipi_20260707_063745/007_add_f2_1525_flatte"
+
+export MPLCONFIGDIR=/tmp/matplotlib_cache_${USER:-condor}_$$
+mkdir -p "${MPLCONFIGDIR}"
+export PYTHON_BIN="/afs/cern.ch/work/h/hwen/public/conda/envs/tfpwa/bin/python3.12"
+
+bash run_fit_witherr_fs.sh
---
+++ Jobs/scan_phipipi_20260707_063755/000_baseline/run_condor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euo pipefail
+
+cd "/afs/cern.ch/work/h/hwen/public/pwa/auto-init/Jobs/scan_phipipi_20260707_063755/000_baseline"
+
+export MPLCONFIGDIR=/tmp/matplotlib_cache_${USER:-condor}_$$
+mkdir -p "${MPLCONFIGDIR}"
+export PYTHON_BIN="/afs/cern.ch/work/h/hwen/public/conda/envs/tfpwa/bin/python3.12"
+
+bash run_fit_witherr_fs.sh
